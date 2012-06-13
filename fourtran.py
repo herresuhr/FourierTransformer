45b393d2490c4f2dc4b20fc399c16ac42fe6f533
Troels Suhr <herresuhr@gmail.com>
2012-06-13 11:16:29 +0200

    Added fitting routine for multiple truncated harmonic oscillation

--- fourtran.py
+++ fourtran.py
@@ -6,6 +6,9 @@
 #	transform, continous fourier transform or harmonic inversion. Outputs transformed data
 #	and estimated Q factors.
 #
+#   Added routine for fitting multiple peaks to a truncated harmonic oscillation. 
+#   Generally finds peaks nicely, but fitting routine occasionally greatly overestimates 
+#   Q if the fitted FWHM is below the spectral resolution.
 
 import argparse
 import numpy as np
@@ -13,6 +16,7 @@ from scipy import optimize
 #from scipy import *
 from math import *
 import matplotlib.pyplot as plt
+import movingaverage as ma
 
 def FT(t,x):
 	# Continous Fourier Transform module
@@ -83,93 +87,274 @@ def HI():
 	
 	print "-"*40 + "\n"	
 
+def findTQ(freq, fx, peaks=1, t=-1):
+	# Fits truncated lorentzian to input data using Scipy optimize function
+	
+	print "-"*40 + "\n"
+	print "Fitting lorentzian to spectrum\n"
+	
+	if t is -1:
+		t = 1/(freq[1]-freq[0])
+	
+	fitfunc = lambda p, x: p[0]/4.0*(1 - 2*np.exp(-abs(p[2])*t)*np.cos((x-p[1])*t) + np.exp(-2*abs(p[2])*t))/((x-p[1])**2 + p[2]**2)
+	errfunc = lambda p, x, y: (fitfunc(p,x) - y)
+	
+	if type(peaks) is not np.ndarray:
+		peaks = np.array([peaks])
+	
+	P = {}
+	for i in range(len(peaks)):
+		p0 = [1e3, peaks[i], 1e-3]
+
+		p, success = optimize.leastsq(errfunc, p0[:], args=(freq, fx))
+
+		P[i] = (p)
+		print t*p[2]
+		
+	fmean = P[0][1]
+	df = 0.2*fmean
+				
+	if len(P) > 1:
+		fmin  = min(P[i][1] for i in P.keys())
+		fmax  = max(P[i][1] for i in P.keys())
+		fmean = (fmin + fmax)/2
+		df    = (fmax - fmin)/2
+		if df < 1e-2:
+			df = 0.2*fmean
+		
+	ff = np.linspace(fmean - 2*df,fmean + 2*df,1e4)
+	
+	plt.figure()
+	plt.plot(freq, fx, 'bx')
+	plt.xlim([fmean - 2*df,fmean + 2*df])
+	plt.xlabel('freq')
+	plt.ylabel('amplitude')
+	plt.title('Fitted data')
+	plt.hold('on')
+	for i in P.keys():
+		plt.plot(ff,fitfunc(P[i],ff),'r')
+	plt.show()
+	
+	print "Found resonances:\n"
+	for key in P.keys():
+		print "Peak No " + str(key)
+		print "Amplitude\t = " + str(P[key][0])
+		print "Resonance\t = " + str(P[key][1])
+		print "Q-factor\t = "  + str(abs(P[key][1]/(2*pi*P[key][2]))) + "\n"
+	
+	print "-"*40 + "\n"
+	
+	return P
+
 def findQ(freq, fx, peaks=1):
 	# Fits lorentzian to input data using Scipy optimize function
 	
 	print "-"*40 + "\n"
 	print "Fitting lorentzian to spectrum\n"
 	
-	fitfunc = lambda p, x: p[0]*(p[1]/(4*pi*p[2]))**2/((x-p[1])**2 + (p[1]/(4*pi*p[2]))**2)
-	errfunc = lambda p, x, y: fitfunc(p,x) - y
+	fitfunc = lambda p, x: p[0]**2*(p[1]/(2*p[2]))**2/((x-p[1])**2 + (p[1]/(2*p[2]))**2)
+	errfunc = lambda p, x, y: (fitfunc(p,x) - y)*fitfunc(p,x)
 	
-	P = []
-	for i,p in enumerate(peaks):
-		p0 = [1e6, p, 1000]
+	if type(peaks) is not np.ndarray:
+		peaks = np.array([peaks])
+	
+	P = {}
+	for i in range(len(peaks)):
+		p0 = [1e3, peaks[i], 100]
+
 		p, success = optimize.leastsq(errfunc, p0[:], args=(freq, fx))
-		P.append(p)
+		P[i] = (p)
 		
+	for key in P.keys():
+		if abs(P[key][0]) < 1e-5:
+			print P[key][0]
+			P.pop(key)
+				
+	if len(P) is 1:
+		fmean = P[0][1]
+		df    = 0.2*fmean
+
 	if len(P) > 1:
 		fmin  = min(P[i][1] for i in range(len(P)))
 		fmax  = max(P[i][1] for i in range(len(P)))
 		fmean = (fmin + fmax)/2
 		df    = (fmax - fmin)/2
+		if df < 1e-2:
+			df = 0.2*fmean
 		
-	if len(P) is 1 or df < 1e-2:
-		fmean = P[0][1]
-		df    = 0.2*fmean
+	
+
+	ff = np.linspace(0,17,1e4)
 	
 	plt.figure()
-	plt.plot(freq, fx, 'b')
+	plt.plot(freq, fx, 'bx')
 	plt.xlim([fmean - 1.1*df,fmean + 1.1*df])
 	plt.xlabel('freq')
 	plt.ylabel('amplitude')
 	plt.title('Fitted data')
 	plt.hold('on')
 	for i in range(len(P)):
-		plt.plot(freq,fitfunc(P[i],freq),'rx')
+		plt.plot(ff,fitfunc(P[i],ff),'rx')
 	plt.show()
 	
-	print "Found resonances:"
-	for i,v in enumerate(P):
-		print "Peak No " + str(i)
-		print "Amplitude\t = " + str(v[0])
-		print "Resonance\t = " + str(v[1])
-		print "Q-factor\t = "  + str(abs(v[2])) + "\n"
+	print "Found resonances:\n"
+	for key in P.keys():
+		print "Peak No " + str(key)
+		print "Amplitude\t = " + str(P[key][0])
+		print "Resonance\t = " + str(P[key][1])
+		print "Q-factor\t = "  + str(abs(P[key][2])) + "\n"
 	
 	print "-"*40 + "\n"
 	
-	return p
+	return P
+
+def findMTQ(freq, fx, peaks=1, t=-1):
+	# Fits truncated double lorentzian to input data using Scipy optimize function
 	
-def findpeaks(freq,fx):
+	print "-"*40 + "\n"
+	print "Fitting multiple truncated lorentzians to spectrum\n"
+
+	if t is -1:
+		t = 1/(freq[1]-freq[0])
+	
+	def fitfunc(p,x):
+		N = len(p)/3
+		
+		# Pull out amplitudes, resonance frequencies and decay constants
+		# Absolute value due to fitting crossing zero
+		A = abs(p[0::3])
+		w = 2*pi*abs(p[1::3])
+		a = 2*pi*abs(p[2::3])
+		x = 2*pi*x
+	
+		fit = 0
+		for n in range(N):
+			for m in range(N):
+				NR = (a[n]*a[m]+(x-w[n])*(x-w[m]))*(1 + np.exp(-(a[n]+a[m])*t)*np.cos((w[m]-w[n])*t) - np.exp(-a[n]*t)*np.cos((x-w[n])*t) - np.exp(-a[m]*t)*np.cos((x-w[m])*t))
+				NI = (a[n]*(x-w[m]) - a[m]*(x-w[n])) * (np.exp(-(a[n]+a[m])*t)*np.sin((w[m]-w[n])*t) - np.exp(-a[n]*t)*np.sin((x-w[n])*t) + np.exp(-a[m]*t)*np.sin((x-w[m])*t))
+				DN = (a[n]*a[m] + (x-w[n])*(x-w[m]))**2 + (a[n]*(x-w[m])-a[m]*(x-w[n]))**2
+				fit += A[n]*A[m]/2.0*a[n]*a[m]*(NR+NI)/DN
+				
+		return fit
+		
+	errfunc = lambda p, x, y: (fitfunc(p,x) - y)
+	
+	if type(peaks) is not np.ndarray:
+		peaks = np.array([peaks])
+	
+	p0 = 3*len(peaks)*[0]
+	p0[0::3] = len(peaks)*[1e3]
+	for n in range(len(peaks)): 
+		p0[1+3*n] = peaks[n]
+	p0[2::3] = len(peaks)*[1e-3]
+	
+	P, success = optimize.leastsq(errfunc, p0[:], args=(freq, fx))
+
+	#P_max = max(P[::3])
+	#P[::3] = P[::3]/P_max
+	#fx = fx/P_max
+		
+	fmean = (peaks[-1] + peaks[0])/2
+	df    = (peaks[-1] - peaks[0])/2
+		
+	ff = np.linspace(fmean - 2*df,fmean + 2*df,1e4)
+	
+	plt.figure()
+	plt.plot(freq, fx, 'bx',ff,fitfunc(P,ff),'r')
+	plt.xlim([fmean - 2*df,fmean + 2*df])
+	plt.xlabel('freq [c/a]')
+	plt.ylabel('amplitude')
+	plt.title('Fitted data')
+	plt.show()
+	
+	print "Found resonances:\n"
+	for n in range(len(peaks)):
+		print "Peak No " + str(n)
+		print "Amplitude\t = " + str(round(P[3*n],4))
+		print "Resonance\t = " + str(round(P[3*n+1],4))
+		print "Q-factor\t = "  + str(round(abs(P[3*n+1]/P[3*n+2]),1)) + "\n"
+	
+	print "-"*40 + "\n"
+	
+	return P	
+	
+def findpeaks(freq,fx,ran=-1):
 	# Identifies peaks of a given data by inspecting the 1st derivative
 	
 	print "-"*40 + "\n"
 	print "Identifying spectrum peaks\n"
 	
+	if ran is -1:
+		ran = [0,freq[-1]]
+	
 	dfx = np.diff(fx)
 	
 	npeaks = []
-	for i in range(1,len(dfx)-1):
-		if dfx[i] > dfx[i-1] and dfx[i] > dfx[i+1]:
-			npeaks.append(i+2)
+	for i in range(1,len(dfx)):
+		if freq[i] >= ran[0] and freq[i] <= ran[1] and dfx[i-1] > 0 and dfx[i] < 0:
+			npeaks.append(i)
 	
-	fpeaks = freq[npeaks]
+	A_max = max(fx[npeaks])
+	tmp = npeaks
+	npeaks = []
+	for n in tmp:
+		if fx[n]/A_max > 1e-2:
+			npeaks.append(n)
 	
-	plt.figure()
-	plt.plot(freq,fx,'b',fpeaks,fx[npeaks],'ro')
-	plt.xlabel('frequency')
-	plt.ylabel('amplitude')
-	plt.title('Peaks')
-	plt.show()
+	peaks = freq[npeaks]
+
+#	plt.figure()
+#	plt.plot(freq,fx,'b',fpeaks,fx[npeaks],'ro')
+#	plt.xlabel('frequency')
+#	plt.ylabel('amplitude')
+#	plt.title('Peaks')
+#	plt.show()
 	
-	print "-"*40 + "\n"
-	print "Found " + str(len(fpeaks)) + " peaks:"
+	print "Found " + str(len(peaks)) + " peaks:\n"
 	print "Peak\tfrequency"
-	for i,p in enumerate(fpeaks):
-		print str(i) + "\t" + str(p)
+	for i,p in enumerate(peaks):
+		print str(i) + "\t" + str(round(p,4))
 	
 	print "\n"	
 	print "-"*40 + "\n"
 	
-	return fpeaks
+	return peaks
+
+def avdata(data,win):
+
+	avd = ma.smoothListGaussian(data,win)
+	
+	plt.figure()
+	plt.plot(data,'r')
+	plt.hold()
+	plt.plot(avd,'b')
+	plt.show()
 
 def main():
 	
 	plt.close('all')
+		
+	# Constructed test data
+#	t = np.linspace(0,1e3,1e4)
+#	x = np.sin(2*pi*t)*np.exp(-0.01*t)
+#	x = np.sin(2*pi*0.23*t)*np.exp(-2*pi*0.23/100*t) + np.sin(2*pi*0.26*t)*np.exp(-2*pi*0.26/100*t)
 	
-	t = np.linspace(0,1e3,2e4)
-	#x = np.sin(2*pi*t)*np.exp(-0.01*t)
-	x = np.sin(2*pi*t)*np.exp(-0.01*t) + np.sin(1.01*2*pi*t)*np.exp(-0.01*t)
+	
+	
+#	fid = open('data.txt')
+#	fid = open('L3mod_f02516_FFcen_Ey_re.txt')
+	fid = open('BHS_r030_M0_re.txt')
+	fid = open('BHS_r034_longrun_M0_re.txt')
+		
+	x = []
+	for line in fid.readlines():
+		x.append(float(line))
+
+	fid.close()
+
+	x = x[1500:]
+#	print len(x)
+	t = np.linspace(0,len(x)/32.,len(x))
 	
 	plt.figure()
 	plt.plot(t,x)
@@ -178,15 +363,21 @@ def main():
 	plt.title('Time series')
 	plt.show()
 	
-	#freq,fx = FT(t,x)
+	freq,fx = FFT(t,x)
 	
-	freq2,fx2 = FFT(t,x)
+	peaks = findpeaks(freq,abs(fx)**2,ran=[0.19,0.31])
 	
-	fpeaks = findpeaks(freq2,abs(fx2)**2)
+	P = findMTQ(freq,abs(fx)**2,peaks=peaks,t=2*t[-1])
 	
-	#A,res,Q = findQ(freq,abs(fx)**2)
-	
-	A2,res2,Q2 = findQ(freq2,abs(fx2)**2,peaks=fpeaks)
+	newpeaks = np.array([])
+	for n in range(2,len(P),3):
+		if P[n] > (freq[1] - freq[0])/10.0:
+			newpeaks = np.append(newpeaks,P[n-1])
 
+
+	if len(peaks) > len(newpeaks):
+		Q = findMTQ(freq,abs(fx)**2,peaks=newpeaks,t=2*t[-1])
+	
+			
 if __name__ == '__main__':
 	main()
